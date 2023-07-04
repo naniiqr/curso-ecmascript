463a37cbe02868ac293dbd214ff5ca57be33782a
Daniella Quijada <Daneq152@gmail.com>
2023-07-04 15:30:15 -0400

    First examples of the changes that came with ECMAScript6

---
+++ src/es6/03-strings.js
@@ -0,0 +1,20 @@
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world + "!";
+
+
+//template literals (se hacen con comillas francesas)
+
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+//Multi-line strings
+
+let lorem = "esto es un string \n" + "esto es otra linea";
+
+let lorem2 = `esta es una frase epica
+la continuacion de una frase epica
+`;
+
+console.log(lorem);
+console.log(lorem2);
