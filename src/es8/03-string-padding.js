24114270b875427d5165b49dac3cb12cd9ae0b89
Daniella Quijada <Daneq152@gmail.com>
2023-07-11 13:17:25 -0400

    We open the es8 folder where new features such as object entries and values, string paddings and more are explained

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,6 @@
+const string = 'Hello';
+
+console.log(string.padStart(6, '_'));
+console.log(string.padEnd(6, '_'));
+
+// string padding nos permite rellenar los espacios faltantes, en el primer parametro va la cantidad de caracteres y en el segundo con que se va a rellenar
---
+++ src/es8/04-trailling-commas.js
@@ -0,0 +1,5 @@
+const array = [24, 34, 25, 24, , , , , 45];
+console.log(array);
+console.log(array.length);
+
+// Las comas nos permiten reservar espacios es decir se cuentan como items vacios. 
